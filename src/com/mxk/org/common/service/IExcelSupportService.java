127cf8bded4befe29e467c9eca868d7f9f83825e
liuyijiang <liuyijiang3430@qq.com>
2013-07-01 10:49:50 +0800

    dwadawd

--- src/com/mxk/org/common/service/IExcelSupportService.java
+++
@@ -1,9 +0,0 @@
-package com.mxk.org.common.service;
-
-public interface IExcelSupportService {
-   
-	 public String createProjectLogExcel(String pid,String pname) throws Exception;
-	
-	 public String createProjectMaterExcel(String pid,String pname) throws Exception;
-	 
-}
